79edff328a4f83be18a3b208004216ec9ca2a142
polivetrova <itspollyv@gmail.com>
2022-09-23 14:42:49 +0300

    rx is now used the right way (presumably) changed Flowable to Single in api requests

--- app/src/main/java/com/pet/picker/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/pet/picker/model/repository/RepositoryImpl.kt
@@ -3,35 +3,24 @@ package com.pet.picker.model.repository
 import com.pet.picker.model.entities.PhotoDTO
 import com.pet.picker.model.entities.UnsplashPhoto
 import com.pet.picker.unsplashAPI.SearchRepo
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.core.Single
 
 class RepositoryImpl : Repository {
 
-    override fun getSearchResultsFor(query: String): List<UnsplashPhoto> {
+    override fun getResultsFor(query: String): Single<List<UnsplashPhoto>> {
 
-        val data = mutableListOf<UnsplashPhoto>()
-        val dto: Flowable<PhotoDTO> = SearchRepo.api.getSearchResult(query)
-        dto.onBackpressureLatest()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .map { photoList ->
-                photoList.results.forEach { photo ->
-                    data.add(
-                        UnsplashPhoto(
-                            id = photo.id ?: "N/A",
-                            likes = photo.likes ?: 0,
-                            username = photo.user.username ?: "N/A",
-                            linkFull = photo.urls.full ?: "N/A",
-                            linkRegular = photo.urls.regular ?: "N/A",
-                            linkThumb = photo.urls.thumb ?: "N/A"
-                        )
-                    )
-                }
+        val dto: Single<PhotoDTO> = SearchRepo.api.getSearchResult(query)
+        return dto.map { photoList ->
+            photoList.results.map { photo ->
+                UnsplashPhoto(
+                    id = photo.id ?: "N/A",
+                    likes = photo.likes ?: 0,
+                    username = photo.user.username ?: "N/A",
+                    linkFull = photo.urls.full ?: "N/A",
+                    linkRegular = photo.urls.regular ?: "N/A",
+                    linkThumb = photo.urls.thumb ?: "N/A"
+                )
             }
-            .doOnError { e -> e.printStackTrace() }
-            .subscribe()
-        return data
+        }
     }
 }
--- app/src/main/java/com/pet/picker/ui/SearchFragmentViewModel.kt
+++ app/src/main/java/com/pet/picker/ui/SearchFragmentViewModel.kt
@@ -6,17 +6,32 @@ import androidx.lifecycle.ViewModel
 import com.pet.picker.model.AppState
 import com.pet.picker.model.repository.Repository
 import com.pet.picker.model.repository.RepositoryImpl
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
 
 class SearchFragmentViewModel : ViewModel() {
 
     private val localLiveData: MutableLiveData<AppState> = MutableLiveData()
     val searchResultsLiveData: LiveData<AppState> get() = localLiveData
     private val repository: Repository = RepositoryImpl()
+    private val disposables: CompositeDisposable = CompositeDisposable()
 
     fun getSearchResultsFor(query: String) {
 
-        localLiveData.value = AppState.Loading
-        val data = repository.getSearchResultsFor(query)
-        localLiveData.value = AppState.Success(data)
+        val disposable = repository.getResultsFor(query)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { localLiveData.value = AppState.Loading }
+            .subscribe(
+                { photos -> localLiveData.value = AppState.Success(photos) },
+                { error: Throwable -> localLiveData.value = AppState.Error(error) }
+            )
+        disposables.add(disposable)
+    }
+
+    override fun onCleared() {
+        disposables.clear()
     }
 }
