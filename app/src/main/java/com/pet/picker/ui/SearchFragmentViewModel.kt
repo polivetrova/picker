faefa79912a54dcf08670bdc952126b2bafc28b8
polivetrova <itspollyv@gmail.com>
2022-11-06 21:12:40 +0300

    removed the search button in favor of search-while-typing feature

--- app/src/main/java/com/pet/picker/ui/SearchFragmentViewModel.kt
+++ app/src/main/java/com/pet/picker/ui/SearchFragmentViewModel.kt
@@ -7,8 +7,11 @@ import com.pet.picker.model.repository.RepositoryImpl
 import com.pet.picker.plusAssign
 import io.reactivex.rxjava3.core.BackpressureStrategy
 import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class SearchFragmentViewModel : ViewModel() {
 
@@ -18,15 +21,30 @@ class SearchFragmentViewModel : ViewModel() {
     private val appStateSubject: BehaviorSubject<AppState> = BehaviorSubject.create()
     val appStateFlowable: Flowable<AppState> =
         appStateSubject.toFlowable(BackpressureStrategy.LATEST)
+    private val querySubject: PublishSubject<String> = PublishSubject.create()
 
-    fun getSearchResultsFor(query: String) {
+    init {
+        getSearchResultsFor()
+    }
 
-        disposables += repository.getPhotos(query)
-            .doOnSubscribe { appStateSubject.onNext(AppState.Loading) }
-            .subscribe(
-                { photos -> appStateSubject.onNext(AppState.Success(photos)) },
-                { error: Throwable -> appStateSubject.onNext(AppState.Error(error)) }
-            )
+    fun onQueryChanged(query: String) {
+        querySubject.onNext(query)
+    }
+
+    private fun getSearchResultsFor() {
+
+        disposables += querySubject
+            .debounce(300, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .switchMap { query -> Observable.just(query) }
+            .subscribe { query ->
+                repository.getPhotos(query)
+                    .doOnSubscribe { appStateSubject.onNext(AppState.Loading) }
+                    .subscribe(
+                        { photos -> appStateSubject.onNext(AppState.Success(photos)) },
+                        { error: Throwable -> appStateSubject.onNext(AppState.Error(error)) }
+                    )
+            }
     }
 
     override fun onCleared() {
